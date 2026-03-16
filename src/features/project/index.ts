f9c1fd6eaf40348a2728aab2985ba010581c3c23
lsk <lsk@tt.com>
2026-03-16 17:23:26 +0800

    feat: update project feature exports

--- src/features/project/index.ts
+++ src/features/project/index.ts
@@ -1,4 +1,4 @@
 export * from './api'
-export { ProjectCard } from './components/ProjectCard'
-export { default as ProjectDetail } from './components/ProjectDetail'
+export * from './components'
 export * from './hooks'
+export * from './types'
